14500bf17a8a5d362658dd773e301ecf890915f6
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-11-12 22:20:12 -0600

    Added render logic to map markers. Added styling to map marker/popups

--- client/src/components/Mapbox.js
+++ client/src/components/Mapbox.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
-import ReactMapGL, { Layer, Source, Marker } from "react-map-gl";
+import ReactMapGL, { Layer, Source, Marker, Popup } from "react-map-gl";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import geoJSON from "../json/geoJSON.json";
 import "../css/mapbox.css";
 import CustSpinner from "./layout/CustSpinner";
@@ -14,6 +15,9 @@ const Mapbox = () => {
   const { width, height } = useWindowDimensions();
   const [sources, setSources] = useState([]);
   const [markers, setMarkers] = useState([]);
+  const [markersModal, setMarkersModal] = useState([]);
+  const [popupJustClosed, setPopupJustClosed] = useState(false);
+  const [markerJustMoved, setMarkerJustMoved] = useState(false);
 
   let geoJSONRegions = [];
 
@@ -62,8 +66,35 @@ const Mapbox = () => {
     zoom: 3,
   });
 
+  const addMarker = ([longitude, latitude]) => {
+    setMarkers(markers => [...markers, { longitude, latitude }]);
+    setMarkersModal(markersModal => markersModal.concat(false));
+  };
+
+  const handleMarkerDrag = ([longitude, latitude], index) => {
+    setMarkers(prevMarkers =>
+      prevMarkers.map((m, i) => (index !== i ? m : { longitude, latitude }))
+    );
+    setMarkerJustMoved(true);
+    setTimeout(() => setMarkerJustMoved(false), 100);
+  };
+
+  const handleMarkerClick = index => {
+    if (!markerJustMoved) {
+      setMarkersModal(prevMarkersModal =>
+        prevMarkersModal.map((markerModal, i) =>
+          index === i ? !markerModal : markerModal
+        )
+      );
+      setPopupJustClosed(true);
+      setTimeout(() => setPopupJustClosed(false), 400);
+    }
+  };
+
   if (isAuth && JSON.stringify(profile) === "{}")
     return <Redirect to="/create" />;
+
+  console.log(markerJustMoved);
 
   return (
     <>
@@ -79,31 +110,68 @@ const Mapbox = () => {
             onViewportChange={nextViewport => setViewport(nextViewport)}
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
             className="w-full"
-            onClick={({ lngLat: [longitude, latitude] }) =>
-              setMarkers(markers => [...markers, { longitude, latitude }])
-            }
+            onClick={({ lngLat }) => !popupJustClosed && addMarker(lngLat)}
           >
             {sources}
             <Checklist />
+
             {markers.map((m, index) => (
-              <Marker
-                key={index}
-                {...m}
-                offsetLeft={-23}
-                offsetTop={-23}
-                draggable
-                onDragEnd={({ lngLat: [longitude, latitude] }) =>
-                  setMarkers(prevMarkers =>
-                    prevMarkers.map((m, i) =>
-                      index !== i ? m : { longitude, latitude }
-                    )
-                  )
-                }
-              >
-                <i className="gen-btn fa fa-globe" aria-hidden="true">
-                  <div className="navbrand-icon"></div>
-                </i>
-              </Marker>
+              <React.Fragment key={index}>
+                <div onClick={() => handleMarkerClick(index)}>
+                  <Marker
+                    {...m}
+                    offsetLeft={-23}
+                    offsetTop={-23}
+                    draggable
+                    onClick={index => handleMarkerClick(m, index)}
+                    onDragEnd={({ lngLat }) => handleMarkerDrag(lngLat, index)}
+                  >
+                    <i className="gen-btn fa fa-globe" aria-hidden="true">
+                      <div className="navbrand-icon" />
+                    </i>
+                  </Marker>
+                </div>
+                {markersModal[index] && (
+                  <Popup
+                    {...m}
+                    tipSize={10}
+                    anchor="bottom"
+                    closeButton={false}
+                    closeOnClick={false}
+                    offsetTop={-10}
+                    onClose={() => handleMarkerClick(index)}
+                  >
+                    <div className="popup-inner">
+                      <div className="popup-btn-grp">
+                        <button type="button">
+                          <i
+                            className="fa fa-trash fa-popup fa-popup-trash"
+                            aria-hidden="true"
+                          ></i>
+                        </button>
+                        <div>
+                          <button type="button">
+                            <i
+                              className="fa fa-pencil fa-popup fa-popup-pencil"
+                              aria-hidden="true"
+                            ></i>
+                          </button>
+                          <button
+                            type="button"
+                            onClick={() => handleMarkerClick(index)}
+                          >
+                            <i
+                              className="fa fa-times fa-popup fa-popup-times"
+                              aria-hidden="true"
+                            ></i>
+                          </button>
+                        </div>
+                      </div>
+                      <div>Texttestestestestestsetestes</div>
+                    </div>
+                  </Popup>
+                )}
+              </React.Fragment>
             ))}
           </ReactMapGL>
         </div>
--- utils/genAccessToken.js
+++ utils/genAccessToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const genAccessToken = userID =>
-  jwt.sign(userID, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+  jwt.sign(userID, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
 
 module.exports = genAccessToken;
