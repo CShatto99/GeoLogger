ab615c2ae5b810de486f80e64fa93d68c1cd142a
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2021-07-13 23:49:40 -0500

    Fixed expanded image limits. Fixed marker popup delete hover. Made slight changes on public profile page.

--- client/src/components/profile/publicProfile/PhotoAlbum.tsx
+++ client/src/components/profile/publicProfile/PhotoAlbum.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 import { MarkerType } from '../../../store/types';
+import { DefaultLink } from '../../common/Links';
 import Divider from '../../common/styles/Divider';
 import Photo from './Photo';
 
@@ -28,15 +29,25 @@ const Photos = styled.div`
 
 type PhotoAlbumProps = {
   markers: MarkerType[];
+  userViewingSelf: boolean;
+  username: string;
 };
 
-const PhotoAlbum: FC<PhotoAlbumProps> = ({ markers }: PhotoAlbumProps) => {
+const PhotoAlbum: FC<PhotoAlbumProps> = ({ markers, userViewingSelf, username }: PhotoAlbumProps) => {
+  const noPhotosMsg = userViewingSelf ? (
+    <h3>
+      You have no photos. Add a photo by <DefaultLink to="/map"> creating a marker</DefaultLink>.
+    </h3>
+  ) : (
+    <h3>{username} has no public photos.</h3>
+  );
+
   return (
     <PhotoAblumContainer>
       <h1>Photo Album</h1>
       <Divider />
       {markers.length <= 0 ? (
-        <h3>This user has no images uploaded :(</h3>
+        noPhotosMsg
       ) : (
         <Photos>
           {markers.map((m: MarkerType) => <Photo key={m._id} title={m.title} image={m.image} />).reverse()}
